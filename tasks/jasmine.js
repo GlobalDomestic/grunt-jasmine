8539e611cbc571a9e490f7c7745f7fe2614000c2
GlobalDomestic <jason.cust@gmail.com>
2014-02-26 13:57:55 -0500

    fix: expose jasmine to global and allow multiple runs of specs (useful for grunt-contrib-watch)

--- tasks/jasmine.js
+++ tasks/jasmine.js
@@ -9,58 +9,6 @@
 'use strict';
 var util = require('util');
 var path = require('path');
-var jasmineRequire = require('jasmine-core/lib/jasmine-core/jasmine.js');
-var consoleFns = require('jasmine-core/lib/console/console.js');
-var jasmine = jasmineRequire.core(jasmineRequire);
-
-extend(jasmineRequire, consoleFns);
-jasmineRequire.console(jasmineRequire, jasmine);
-
-var env = jasmine.getEnv();
-
-var jasmineInterface = {
-  describe: function describe(description, specDefinitions) {
-    return env.describe (description, specDefinitions);
-  },
-  xdescribe: function xdescribe(description, specDefinitions) {
-    return env.xdescribe(description, specDefinitions);
-  },
-  it: function it(desc, func) {
-    return env.it(desc, func);
-  },
-  xit: function xit(desc, func) {
-    return env.xit(desc, func);
-  },
-  beforeEach: function beforeEach(beforeEachFunction) {
-    return env.beforeEach(beforeEachFunction);
-  },
-  afterEach: function afterEach(afterEachFunction) {
-    return env.afterEach(afterEachFunction);
-  },
-  expect: function expect(actual) {
-    return env.expect(actual);
-  },
-  spyOn: function spyOn(obj, methodName) {
-    return env.spyOn(obj, methodName);
-  },
-  jsApiReporter: new jasmine.JsApiReporter({
-    timer: new jasmine.Timer()
-  })
-};
-
-jasmine.addCustomEqualityTester = function addCustomEqualityTester(tester) {
-  env.addCustomEqualityTester(tester);
-};
-
-jasmine.addMatchers = function addMatchers(matchers) {
-  return env.addMatchers(matchers);
-};
-
-jasmine.clock = function clock() {
-  return env.clock;
-};
-
-extend(global, jasmineInterface);
 
 module.exports = function(grunt) {
 
@@ -73,12 +21,58 @@ module.exports = function(grunt) {
     var options = this.options({
       showColors: true
     });
+    var consoleFns = require('jasmine-core/lib/console/console.js');
+    var jasmineRequire = require('jasmine-core/lib/jasmine-core/jasmine.js');
+    var jasmine = jasmineRequire.core(jasmineRequire);
 
-    global.window = {
-      setTimeout: setTimeout,
-      clearTimeout: clearTimeout,
-      setInterval: setInterval,
-      clearInterval: clearInterval
+    extend(jasmineRequire, consoleFns);
+    jasmineRequire.console(jasmineRequire, jasmine);
+
+    var env = jasmine.getEnv();
+
+    var jasmineInterface = {
+      describe: function describe(description, specDefinitions) {
+        return env.describe(description, specDefinitions);
+      },
+      xdescribe: function xdescribe(description, specDefinitions) {
+        return env.xdescribe(description, specDefinitions);
+      },
+      it: function it(desc, func) {
+        return env.it(desc, func);
+      },
+      xit: function xit(desc, func) {
+        return env.xit(desc, func);
+      },
+      beforeEach: function beforeEach(beforeEachFunction) {
+        return env.beforeEach(beforeEachFunction);
+      },
+      afterEach: function afterEach(afterEachFunction) {
+        return env.afterEach(afterEachFunction);
+      },
+      expect: function expect(actual) {
+        return env.expect(actual);
+      },
+      spyOn: function spyOn(obj, methodName) {
+        return env.spyOn(obj, methodName);
+      },
+      jsApiReporter: new jasmine.JsApiReporter({
+        timer: new jasmine.Timer()
+      }),
+      jasmine: jasmine
+    };
+
+    extend(global, jasmineInterface);
+
+    jasmine.addCustomEqualityTester = function addCustomEqualityTester(tester) {
+      env.addCustomEqualityTester(tester);
+    };
+
+    jasmine.addMatchers = function addMatchers(matchers) {
+      return env.addMatchers(matchers);
+    };
+
+    jasmine.clock = function clock() {
+      return env.clock;
     };
 
     // Iterate over all specified file groups.
@@ -86,21 +80,24 @@ module.exports = function(grunt) {
       var filepath = path.resolve(file);
       // Load file source.
       require(filepath);
+
+      // Prevent subsequent runs from using cached tests
+      delete require.cache[filepath];
       grunt.verbose.writeln('Loaded spec file: %s', filepath);
     });
 
-    var jasmineEnv = jasmine.getEnv();
     var consoleReporter = new jasmine.ConsoleReporter({
       showColors: options.showColors,
       print: grunt.log.write,
       timer: new jasmine.Timer(),
       onComplete: function onComplete(exitCode) {
+        unextend(global, jasmineInterface);
         done(exitCode);
       }
     });
 
-    jasmineEnv.addReporter(consoleReporter);
-    jasmineEnv.execute();
+    env.addReporter(consoleReporter);
+    env.execute();
   });
 };
 
@@ -115,6 +112,22 @@ function extend(origin, add) {
 
   while (i--) {
     origin[keys[i]] = add[keys[i]];
+  }
+
+  return origin;
+}
+
+function unextend(origin, add) {
+  // Don't do anything if add isn't an object
+  if (!add || typeof add !== 'object') {
+    return origin;
+  }
+
+  var keys = Object.keys(add);
+  var i = keys.length;
+
+  while (i--) {
+    delete origin[keys[i]];
   }
 
   return origin;
